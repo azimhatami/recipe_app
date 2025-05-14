cb0d0b31c26ab480c83eff6f74252ec19e321a3e
Azim Hatami <azimhatami.dev@gmail.com>
2025-05-14 14:04:43 +0330

    Implement recipe model

---
+++ models/recipe.js
@@ -0,0 +1,27 @@
+const { Schema, Types, model } = require('mongoose');
+
+
+const recipeSchema = new Schema({
+  title: {type: String, required: true},
+  ingredients: {type: [String], required: true},
+  instruction: {type: String, required: true}
+  author: {type: Types.ObjectId, ref: 'User'},
+  tags: {type: [String]},
+  ratings: [
+    {
+      user: {type: Types.ObjectId, ref: 'User'},
+      rating: Number
+    },
+  ],
+  comments: [
+    {
+      user: {type: Types.ObjectId, ref: 'User'},
+      comment: String,
+    },
+  ],
+});
+
+const recipeModel = model('Recipe', recipeSchema);
+
+
+module.exports = recipeModel;
