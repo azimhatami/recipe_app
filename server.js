e861faf6cfb93b6bfc57f1bf37ba4c2891f5c8ab
Azim Hatami <azimhatami.dev@gmail.com>
2025-05-14 09:05:01 +0330

    Add initial database configuration

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// DB connection
+connectDB();
 
 app.use(cors());
 
