96a0996459de8a1dd589d8f08b275be73da86590
Azim Hatami <azimhatami.dev@gmail.com>
2025-05-14 11:28:00 +0330

    Implement user login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const bcrypt = require('bcryptjs');
 const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 
+// User Signup
 const signupController = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -20,7 +25,28 @@ const signupController = async (req, res) => {
   }
 };
 
+// User Login
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({email});
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({message: 'wrong credentials...'});
+    }
+
+    const token = jwt.sign(
+      {id: user._id}, 
+      process.env.JWT_SECRET, 
+      {expiresIn: '1h'}
+    );
+    res.status(200).json({ token, message: 'Login successful' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error...' })
+  }
+};
+
 module.exports = {
-  signupController
+  signupController,
+  loginController
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signupController } = require('../controllers/auth');
+const { signupController, loginController } = require('../controllers/auth');
 
 
 router.post('/signup', signupController);
+router.post('/login', loginController);
 
 
 module.exports = router;
