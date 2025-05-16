964b846bf0d4d79eb36ab6b5173bff3129c4b430
Azim Hatami <azimhatami.dev@gmail.com>
2025-05-16 17:45:36 +0330

    Add user follow functionality

--- controllers/user.js
+++ controllers/user.js
@@ -51,9 +51,35 @@ const updateUserProfile = async (req, res) => {
   }
 };
 
+const followTheUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const userTobeFollowed = await userModel.findById(userId);
+    if (!userTobeFollowed) {
+      return res.status(404).json({
+        message: 'User not found'
+      });
+    }
+
+    const user = await userModel.findById(req.user.id);
+    if (!user.following.includes(userTobeFollowed._id)) {
+      user.following.push(userTobeFollowed._id);
+      await user.save();
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Server Error',
+      error: error.message
+    });
+  };
+};
+
 
 module.exports = {
   getAllUsers,
   getUserProfile,
-  updateUserProfile
+  updateUserProfile,
+  followTheUser
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getUserProfile, updateUserProfile } = require('../controllers/user');
+const { getAllUsers, getUserProfile, updateUserProfile, followTheUser } = require('../controllers/user');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
 router.get('/all', authMiddleware, getAllUsers);
 router.get('/profile', authMiddleware, getUserProfile);
 router.put('/profile', authMiddleware, updateUserProfile);
+router.post('/follow/:userId', authMiddleware, followTheUser);
 
 
 module.exports = router;
